b577418ce27cb9c286fc494940ec5e594564c74e
qalmurri <blog.qalmurri@gmail.com>
2026-03-06 10:13:39 +0700

    opt

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,25 +1,8 @@
 return {
   {
-    'echasnovski/mini.map',
+    "echasnovski/mini.map",
     config = function()
-      local map = require('mini.map')
-      map.setup({
-        integrations = {
-            map.gen_integration.gitsigns(),
-            map.gen_integration.diagnostic()
-        },
-        window = {
-            width = 15
-        },
-      })
-      vim.keymap.set(
-          'n',
-          '<leader>mm',
-          map.toggle,
-          {
-              desc = "Toggle Minimap"
-          }
-      )
+      require("mini.map").setup()
     end,
-  }
+  },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,22 +2,52 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" }, -- Load hanya saat buka file agar startup cepat
+    event = { "BufReadPost", "BufNewFile" },
     config = function()
-      local status, treesitter = pcall(require, "nvim-treesitter.configs")
-      if not status then
-        treesitter = require("nvim-treesitter")
-      end
-
-      treesitter.setup({
-        ensure_installed = { "python", "lua", "vim", "vimdoc", "go", "cpp", "bash" },
-        auto_install = true,
-        highlight = {
-          enable = true, -- Aktifkan highlight warna-warni
-          additional_vim_regex_highlighting = false,
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "python",
+          "lua",
+          "vim",
+          "vimdoc",
+          "go",
+          "cpp",
+          "bash",
         },
-        indent = { enable = true }, -- Indentasi otomatis yang lebih pintar
+
+        highlight = {
+          enable = true,
+        },
+
+        indent = {
+          enable = true,
+        },
       })
-    end
+    end,
   },
 }
+
+-- bawaan v0.1
+-- return {
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = ":TSUpdate",
+--     event = { "BufReadPost", "BufNewFile" }, -- Load hanya saat buka file agar startup cepat
+--     config = function()
+--       local status, treesitter = pcall(require, "nvim-treesitter.configs")
+--       if not status then
+--         treesitter = require("nvim-treesitter")
+--       end
+-- 
+--       treesitter.setup({
+--         ensure_installed = { "python", "lua", "vim", "vimdoc", "go", "cpp", "bash" },
+--         auto_install = true,
+--         highlight = {
+--           enable = true, -- Aktifkan highlight warna-warni
+--           additional_vim_regex_highlighting = false,
+--         },
+--         indent = { enable = true }, -- Indentasi otomatis yang lebih pintar
+--       })
+--     end
+--   },
+-- }
